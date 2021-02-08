22cd9f6b0da7ae0830b2854a9d2a8f1b219a95e0
Pete-Scale <petescale@gmail.com>
2021-02-08 16:46:36 -0600

    added department, role, and employee seeds

---
+++ db/seed.sql
@@ -0,0 +1,61 @@
+USE employeeTracker_db;
+
+-- department seeds
+
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Legal");
+
+-- role seeds
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4);
+
+-- employee seeds
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bill", "Murray", 1, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ernie", "Hudson", 1, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Harold", "Ramis", 2, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dan", "Aykroyd", 2, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Rick", "Moranis", 3, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sigourney", "Weaver", 4, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Annie", "Potts", 4, 6);
