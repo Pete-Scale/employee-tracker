1558c7c3e8e38dadfd12443e46000c06adb46bed
Pete-Scale <petescale@gmail.com>
2021-02-08 12:46:25 -0600

    schema creates employeeTracker_db and department, role, and employee tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DELETE DATABASE IF EXISTS employeeTracker_db;
+
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
