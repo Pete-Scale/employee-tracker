da091c0ecc5a590c76242d6528fef4488896ce93
Pete-Scale <petescale@gmail.com>
2021-02-09 00:12:49 -0600

    set up basic mysql and created main function for inquirer

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Employee-Tracker
+
+## Credits
+
+https://www.npmjs.com/package/inquirer/v/0.2.3
+
+https://www.npmjs.com/package/mysql
+
+https://www.npmjs.com/package/console.table
+
+https://javascript.info/task/array-negative
---
+++ server.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employeeTracker_db'
+});
+
+connection.connect(err => {
+    if(err) throw err;
+    console.log(`MySQL connected on ${connection.threadId}`);
+    runEmployeeTracker();
+});
+
+const actionChoices = [
+    "View All Employees",
+    "View All Roles",
+    "View All Departments", 
+    "Add Employee",
+    "Add Role",
+    "Add Department",
+    "Update Employee Roles",
+    "Exit"
+];
+
+function runEmployeeTracker() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: actionChoices
+        })
+        .then(answer => {
+            switch (answer.action) {
+                case actionChoices[0]:
+                    viewAllEmployees();
+                    break;
+                case actionChoices[1]:
+                    viewAllRoles();
+                    break;
+                case actionChoices[2]:
+                    viewAllDepartments();
+                    break;
+                case actionChoices[3]:
+                    addEmployee();
+                    break;
+                case actionChoices[4]:
+                    addRole();
+                    break;
+                case actionChoices[5]:
+                    addDepartment();
+                    break;
+                case actionChoices[6]:
+                    updateEmployeeRole();
+                    break;
+                case actionChoices[-1]:
+                    exitEmployeeTracker();
+                    break;
+            }
+        });
+}
+
+function exitEmployeeTracker() {
+    connection.end();
+    process.exit();
+}
