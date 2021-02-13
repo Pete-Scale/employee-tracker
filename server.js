b2c6ad5381ead7d3713e62750b4f38f02d1bf239
Pete-Scale <petescale@gmail.com>
2021-02-13 09:44:37 -0600

    commented out cTable requirement, normal console.table looks better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const cTable = require('console.table');
+// const cTable = require('console.table');
 
 // CONNECTION -----------------------------------------------------
 const connection = mysql.createConnection({
